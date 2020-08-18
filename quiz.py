c686c022c48bf8dea9e355ed40c8d1a15d4ace57
Jelmer Mulder <jmulder14.jm@gmail.com>
2020-08-18 11:33:21 +0200

    Complete bare-bones version

--- quiz.py
+++ quiz.py
@@ -4,30 +4,59 @@
 # IMPORTS
 from random import randint
 from random import shuffle
-PRICES = ['horse', 'goat', 'goat']
-
 
 # DEFINITIONS
 # shuffles door prics
-def doorgenerator():
+def doorgenerator(PRICES):
     shuffle(PRICES)
 
+
 # first picker
-def prepicker():
+def prepicker(PRICES):
     door = randint(0, 2)
-    pick = PRICES[door]
-    return door, pick
+    PrePick = PRICES[door]
+    return door, PrePick
 
 # quiz master will show goat door
-def quizmasterspick(prenumber):
+def quizmasterspick(prenumber, PRICES):
     masterspick = (prenumber + 1) % 3
-    print(masterspick)
+    if PRICES[masterspick] == 'horse':
+        masterspick = (masterspick + 1) % 3
+    PRICES.pop(masterspick)
+    return PRICES
 
+def loop(PRICES, SecChoice, value):
+    doorgenerator(PRICES)
+    PreNum, PrePick = prepicker(PRICES)
+    if (not SecChoice) and PrePick == 'horse':
+        value += 1
+        return value
+    else:
+        PRICES = quizmasterspick(PreNum, PRICES)
+        if PRICES[randint(0, 1)] == 'horse':
+            value += 1
+            return value
+        else:
+            return value
 
 # main
 if __name__ == "__main__":
-    doorgenerator()
-    prenumber, firstchoice = prepicker()
-    prenumber = 0
-    quizmasterspick(prenumber)
+    SecChoice = True
+    value = 0
+    N = 10000
+    for i in range(0, N):
+        PRICES = ['horse', 'goat', 'goat']
+        value = loop(PRICES, SecChoice, value)
+    change = value / N
+    print('When SecChoice was set {}, the picker changes are {}'.format(SecChoice, change))
+
+    SecChoice = False
+    value = 0
+    N = 100
+    for i in range(0, N):
+        PRICES = ['horse', 'goat', 'goat']
+        value = loop(PRICES, SecChoice, value)
+    change = value / N
+    print('When SecChoice was set {}, the picker changes are {}'.format(SecChoice, change))
+
 
